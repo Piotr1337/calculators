10d5545f1e7d49640c13f5456e44ee9eee84fe96
Piotr1337 <azanwrc@gmail.com>
2017-09-10 20:11:27 +0200

    default-routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { environment } from '../environments/environment';
 import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { CaloriesCalculatorComponent } from './components/calories-calculator/calories-calculator.component';
+import { HomeComponent } from './components/home/home.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CaloriesCalculatorComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFireDatabaseModule
+    AngularFireDatabaseModule,
+    RouterModule.forRoot([
+      {
+        path: 'calories',
+        component: CaloriesCalculatorComponent
+      },
+      {
+        path: '',
+        component: HomeComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
